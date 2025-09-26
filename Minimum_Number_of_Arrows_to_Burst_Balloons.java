c0fde08177e305cf66ed5866744fb4a42bba7f75
kushkumar7668 <kushkumar16052004@gmail.com>
2025-09-26 23:57:10 +0530

    Add files via upload

---
+++ Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -0,0 +1,24 @@
+package Assignment1;
+
+import java.util.Arrays;
+
+public class Minimum_Number_of_Arrows_to_Burst_Balloons {
+    class Solution {
+        public int findMinArrowShots(int[][] points) {
+            Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+            int prev = points[0][1];
+            int count = 1;
+            for (int i = 1; i < points.length; i++) {
+                // if in range then continue
+                if (points[i][0] <= prev) {
+                    continue;
+                } else {
+                    count++;
+                    prev = points[i][1];
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ sliding_window_maximum.java
@@ -0,0 +1,30 @@
+package Assignment1;
+
+import java.util.TreeMap;
+
+public class sliding_window_maximum {
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            int n = nums.length;
+            int[] result = new int[n - k + 1];
+            for (int i = 0; i < k; i++) {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
+
+            result[0] = map.lastKey();
+            for (int i = k; i < n; i++) {
+                int rem = nums[i - k];
+                map.put(rem, map.get(rem) - 1);
+                if (map.get(rem) == 0) {
+                    map.remove(rem);
+                }
+                int add = nums[i];
+                map.put(add, map.getOrDefault(add, 0) + 1);
+                result[i - k + 1] = map.lastKey();
+            }
+
+            return result;
+        }
+    }
+}
